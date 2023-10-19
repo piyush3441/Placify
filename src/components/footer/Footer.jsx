8fdea5fef6ae3993618999a5358964d9f2a7bc51
piyush3441 <piyushsolanki607@gmail.com>
2023-10-19 14:11:19 +0530

    Footer minor changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 
 import "./footer.css";
@@ -9,63 +8,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="row">
-          <div className="footer-contact col-12 py-3 ">
-            <div className="row justify-content-between ">
-              <div className="col-md-4 d-flex">
-                <div className="contact-icon pe-3">
-                  <i className="fa fa-phone"></i>
-                </div>
-                <div className="contact-text">
-                  <h6>+91 94608-68085</h6>
-                  <p>Mon-Fri 9am-6pm</p>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex">
-                <div className="contact-icon pe-3">
-                  <i className="fas fa-envelope"></i>
-                </div>
-                <div className="contact-text">
-                  <h6>info@jklu.com</h6>
-                  <p>Online support</p>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex">
-                <div className="contact-icon pe-3">
-                  <i className="fa fa-map"></i>
-                </div>
-                <div className="contact-text">
-                  <h6>JKLU, Mahapura</h6>
-                  <p>Jaipur Rajasthan</p>
-                </div>
-              </div>
-            </div>
-          </div>
           <div className="footer-social col-12 py-3">
             <div className="row justify-content-center box rounded-0">
-              {/* <div className="col-8 ">
-                <div className="row ">
-                  <div className="col-12 col-md-6 text-center">
-                    <p>Social Media</p>
-                  </div>
-                  <div className="col-12 col-md-6 d-flex justify-content-center align-items-baseline ">
-                    <div className="social-icon me-2">
-                      <i className="fab fa-facebook"></i>
-                    </div>
-                    <div className="social-icon me-2">
-                      <i className="fab fa-twitter"></i>
-                    </div>
-                    <div className="social-icon me-2">
-                      <i className="fab fa-linkedin"></i>
-                    </div>
-                    <div className="social-icon me-2">
-                      <i className="fab fa-github"></i>
-                    </div>
-                    <div className="social-icon me-2">
-                      <i className="fab fa-instagram"></i>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
 
               <div className="col-md-6">
                 <div className="contact-form">
@@ -106,61 +50,39 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          {/* <div className="footer-links col-12 py-3">
-            <div className="row">
-              <div className="links-pages  col-sm-6">
-                <h3>
-                  <Link to="/" className=" text-uppercase fs-3 fw-bold">
-                    <span>lrn</span>plus
-                  </Link>
-                </h3>
-                <ul className="navbar-nav ">
-                  <li className="nav-item  ">
-                    <Link className="nav-link" to="/">
-                      Home
-                    </Link>
-                  </li>
-                  <li className="nav-item  ">
-                    <Link className="nav-link" to="/courses">
-                      Courses
-                    </Link>
-                  </li>
-                  <li className="nav-item  ">
-                    <Link className="nav-link" to="/singlecourse">
-                      Single Course
-                    </Link>
-                  </li>
-                  <li className="nav-item ">
-                    <Link to="signup" className="nav-link">
-                      Sign Up
-                    </Link>
-                  </li>
-                  <li className="nav-item ">
-                    <Link to="/login" className="nav-link">
-                      Log In
-                    </Link>
-                  </li>
-                  <li className="nav-item ">
-                    <Link className="nav-link" to="/contact">
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
+          <hr/>
+          <div className="footer-contact col-12 py-3 ">
+            <div className="row justify-content-between ">
+              <div className="col-md-4 d-flex">
+                <div className="contact-icon pe-3">
+                  <i className="fa fa-phone"></i>
+                </div>
+                <div className="contact-text">
+                  <h6>+91 94608-68085</h6>
+                  <p>Mon-Fri 9am-6pm</p>
+                </div>
               </div>
-              <div className="links-info  col-sm-6 mt-4 mt-sm-0">
-                <h3>learn plus</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestiae natus labore laudantium esse accusantium corporis
-                  delectus rem nam corrupti ipsa velit culpa sint veniam
-                  dignissimos incidunt itaque maiores, sed laborum odio iure.
-                  Illum veniam eius dolorem nisi delectus atque, aliquam facilis
-                  debitis earum illo modi temporibus officiis eveniet ullam
-                  omnis quia{" "}
-                </p>
+              <div className="col-md-4 d-flex">
+                <div className="contact-icon pe-3">
+                  <i className="fas fa-envelope"></i>
+                </div>
+                <div className="contact-text">
+                  <h6>info@jklu.com</h6>
+                  <p>Online support</p>
+                </div>
+              </div>
+              <div className="col-md-4 d-flex">
+                <div className="contact-icon pe-3">
+                  <i className="fa fa-location-dot"  aria-hidden="true"></i>
+                </div>
+                <div className="contact-text">
+                  <h6>JKLU, Mahapura</h6>
+                  <p>Jaipur Rajasthan</p>
+                </div>
               </div>
             </div>
-          </div> */}
+          </div>
+          <hr/>
           <div className="footer-copyright col-12 text-center py-3 ">
             <p>
               &copy;{new Date().getFullYear()} by
